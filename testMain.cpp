f182ebb73819776e45290e6ba78e84c92141d2b7
wangsg <wangsigui@yeah.net>
2016-04-13 09:35:30 +0800

    BinarySearchTree and test

--- testMain.cpp
+++ testMain.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include"Vector.h"
 #include"DoublyLinkedList.h"
-//#include"List.h"
+#include"Stack.h"
+#include"BinarySearchTree.h"
 using namespace std;
 
 int main(){
@@ -25,9 +26,37 @@ int main(){
     douLinkedList.push_back(3);
    for(;itr1 != douLinkedList.end(); itr1++){
          cout << *itr1 << endl;
-   }   
+   }
     while(!douLinkedList.empty()){
          cout << douLinkedList.back() << endl;
          douLinkedList.pop_back();
     }
+
+    Stack<int> s;
+    s.push_back(1);
+    s.push_back(2);
+    s.push_back(3);
+    int b;
+    s.top(b);
+    cout << "Top of stack " << b << endl;
+    while(!s.empty()){
+         cout << s.top() << endl;
+         s.pop();
+    }
+    s.push_back(12);
+    cout << s.top() << endl;
+    s.clear();
+    if(s.empty()){
+         cout << "Stack is empty" << endl;
+    }
+
+    BinarySearchTree<int> binarySearchTree;
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(8);
+
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(3);
+    binarySearchTree.printTree();
 }
